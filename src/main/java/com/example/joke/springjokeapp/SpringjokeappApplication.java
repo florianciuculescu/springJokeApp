bac8ec52c8c3537389b56332fc033d6921cdd496
fciuculescu <fciuculescu@deloittece.com>
2020-08-29 10:51:51 +0300

    first commit

---
+++ src/main/java/com/example/joke/springjokeapp/SpringjokeappApplication.java
@@ -0,0 +1,13 @@
+package com.example.joke.springjokeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringjokeappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringjokeappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/joke/springjokeapp/SpringjokeappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.joke.springjokeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringjokeappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
