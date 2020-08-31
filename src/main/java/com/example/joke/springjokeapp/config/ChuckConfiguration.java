ab6d5bfa113de467aeb7d775cb85a36fd0a8efea
fciuculescu <fciuculescu@deloittece.com>
2020-08-31 22:00:11 +0300

    added Java Based Config class

--- src/main/java/com/example/joke/springjokeapp/config/ChuckConfiguration.java
+++ src/main/java/com/example/joke/springjokeapp/config/ChuckConfiguration.java
@@ -1,2 +1,14 @@
-package com.example.joke.springjokeapp.config;public class ChuckConfiguration {
+package com.example.joke.springjokeapp.config;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ChuckConfiguration {
+
+    @Bean
+    public ChuckNorrisQuotes chuckNorrisQuotes() {
+        return new ChuckNorrisQuotes();
+    }
 }
